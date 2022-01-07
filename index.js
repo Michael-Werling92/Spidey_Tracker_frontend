a5d9bc9acfc4eb7577899956d2f99c2afea20d26
Michael <mfw6mw@virginia.edu>
2022-01-07 17:11:30 -0500

    added DOM Content loaded to console log

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 document.addEventListener("click", (event)=>{ console.log("You Clicked on", event.target) } )
+
+document.addEventListener("DOMContentLoaded", function(){ console.log("DOM IS LISTENING") })
