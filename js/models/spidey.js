ed11a50c1b2538bfb165ae2bf4ab98dbe1dec567
Michael <mfw6mw@virginia.edu>
2022-01-07 17:31:18 -0500

    added rendering to Spidey.js

--- js/models/spidey.js
+++ js/models/spidey.js
@@ -16,11 +16,20 @@ class Spidey{
 
     makeACard =()=>{console.log(this)
 
-            return `
-                <h2 id="Spidey.id" data-id="${this.id}">${this.alias} ${this.surname} ${this.year}</h2>
-                <img src=${this.image} class="car-avatar" />
-                <button data-id="${this.id}" class="delete-btn"> DELETE THIS SPIDEY </button>
-                `
+        return `
+            <h2 id="Spidey.id" data-id="${this.id}">${this.alias} ${this.surname} ${this.year}</h2>
+            <img src=${this.image} class="Spidey-avatar" />
+            <button data-id="${this.id}" class="delete-btn"> DELETE THIS SPIDEY </button>
+            `
         }
     
-        
+    renderSpidey =(spidey)=> {
+            const cardDiv = document.createElement("div")
+            cardDiv.classList.add("card")
+              cardDiv.setAttribute("data-id", spidey.id)
+                cardDiv.id = spidey.id
+                cardDiv.innerHTML = this.makeACard()
+            const collectionDiv = document.querySelector("#gallery")
+            collectionDiv.append(cardDiv)
+        }
+    
