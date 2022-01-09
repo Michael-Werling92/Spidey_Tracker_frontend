c2831677ec9dc90b2d0359287822102f4d29afde
Michael <mfw6mw@virginia.edu>
2022-01-08 19:04:57 -0500

    Added rendering of comic to spideys

--- js/models/comic.js
+++ js/models/comic.js
@@ -2,10 +2,28 @@ console.log ("Record.JS loaded")
 
 class Comic{
     static all = []
-    constructor({id, year, spidey_id}){
+    constructor({id, name, year, spidey_id}){
         this.id = id,
+        this.name = name,
         this.year = year,
         this.spidey_id = spidey_id,
         Record.all.push(this)
     }
+    makeACard =()=>{console.log(this)
+
+        return `
+            Current Universe:
+            <h3>Comic Series: ${this.name} </h3>
+            <h4>Year Active: ${this.year} </h4>
+            `
+        }
+
+    renderComic=(comic)=> {
+        const cardDiv = document.createElement("div")
+        cardDiv.classList.add("card")
+            cardDiv.setAttribute("data-id", comic.id)
+            cardDiv.id = comic.id
+            cardDiv.innerHTML = this.makeACard()
+        const collectionDiv = document.querySelector(`.card[event-id="${comic.spidey_id}"]`)
+        collectionDiv.append(cardDiv)    
 }
